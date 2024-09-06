25856fc15099c63c585ef6cb6a1314c7d6685737
RANA <ruhulaminrana98@gmail.com>
2024-09-07 00:39:19 +0600

    Update and rename npx2.js to Rana2.js

--- scripts/commands/Rana2.js
+++ scripts/commands/Rana2.js
@@ -19,7 +19,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/Yc2atQe.mp4',
+      'https://i.imgur.com/1IeHri1.mp4',
       { responseType: 'stream' }
     )
   ).data;
