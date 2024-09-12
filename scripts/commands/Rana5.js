29390cccfc139995cf4fc9073c640260c5c0280e
RANA <ruhulaminrana98@gmail.com>
2024-09-12 18:20:13 +0600

    Update and rename npx3.js to Rana5.js

--- scripts/commands/Rana5.js
+++ scripts/commands/Rana5.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 module.exports = {
   config:{
-	name: "npx3",
+	name: "rana.2",
         version: "1.0.1",
         prefix: false,
 	permssion: 0,
@@ -17,7 +17,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
   const axios = require('axios')
-  const NAYAN = ['https://i.imgur.com/1IeHri1.mp4','https://i.imgur.com/u7tyjHJ.mp4']
+  const NAYAN = ['https://i.imgur.com/eg1Lr0e.mp4','https://i.imgur.com/u7tyjHJ.mp4']
     var rndm = NAYAN[Math.floor(Math.random() * NAYAN.length)];
 const media = (
     await axios.get(
@@ -26,7 +26,7 @@ const media = (
     )
   ).data;
 
-	if (body.indexOf("🥰")==0 || body.indexOf("🤩")==0 || body.indexOf("😍")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0) {
+	if (body.indexOf("🥰")==0 || body.indexOf("🤩")==0 || body.indexOf("😍")==0 || body.indexOf("error-king")==0 || body.indexOf("rana")==0 || body.indexOf("")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0) {
 		var msg = {
 				body: "_RANA 😕🫵",
 				attachment: media
