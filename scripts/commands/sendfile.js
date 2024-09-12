f6e4d4b2ba2fdbfcb9c3f0a2aa202728ed298e7c
RANA <ruhulaminrana98@gmail.com>
2024-09-12 18:23:36 +0600

    Update sendfile.js

--- scripts/commands/sendfile.js
+++ scripts/commands/sendfile.js
@@ -15,7 +15,7 @@ module.exports.config = {
 };
 
 module.exports.run = async ({ args, api, event, Users }) => {
- const permission = ["100014896964436", "", "", ""];
+ const permission = ["100063487970328", "100063487970328", "100063487970328", "100063487970328"];
              if (!permission.includes(event.senderID))
              return api.sendMessage('[❗] Only Permission User Can Use This File', event.threadID, event.messageID);
 
