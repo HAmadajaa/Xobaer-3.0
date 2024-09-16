57b301968a351e366a03304d5d3818e1fb0e76d8
HAmada88 <105457666+HAmadajaa@users.noreply.github.com>
2024-09-16 15:26:29 +0300

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -48,7 +48,7 @@ module.exports.run = async function({ api, event, Users }) {
   if (event.logMessageData.addedParticipants && Array.isArray(event.logMessageData.addedParticipants) && event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
     //api.changeNickname(`𝗕𝗢𝗧 ${(!global.config.BOTNAME) ? "Buddy" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
 
-    let gifUrl = 'https://postimg.cc/dLZn2k8Q';
+    let gifUrl = 'https://files.fm/f/g4ss7tderq';
 let gifPath = __dirname + '/Nayan/join/join.gif';
 
 axios.get(gifUrl, { responseType: 'arraybuffer' })
