e27bf47c7802b1f0066527105720e5cb615e2b77
HAmada88 <105457666+HAmadajaa@users.noreply.github.com>
2024-09-21 14:32:00 +0300

    Update removebg.js

--- scripts/commands/removebg.js
+++ scripts/commands/removebg.js
@@ -13,7 +13,7 @@ module.exports.run = async function({
     try {
       var tpk = `🖼️=== [ REMOVING BACKGROUND ] ===🖼️
 ━━━━━━━━━━━━━━━
-[⚜️]➜ Api By MOHAMMAD NAYAN`;
+[⚜️]➜ Api By Xobaer`;
         if (event.type !== "message_reply") return api.sendMessage("[⚜️]➜ You must reply to a photo", event.threadID, event.messageID);
         if (!event.messageReply.attachments || event.messageReply.attachments.length == 0) return api.sendMessage("[⚜️]➜ You must reply to a photo", event.threadID, event.messageID);
         if (event.messageReply.attachments[0].type != "photo") return api.sendMessage("[⚜️]➜ This is not an image", event.threadID, event.messageID);
